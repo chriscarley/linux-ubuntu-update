888121430aa2b3c1f1693def9016d52aa80beadc
ctrlaltid <4370587+ctrlaltid@users.noreply.github.com>
2018-10-06 08:43:32 -0400

    Add files via upload

---
+++ Update.sh
@@ -0,0 +1,3 @@
+sudo /bin/sh -c "apt-get -y update && apt-get -y dist-upgrade && apt-get -y autoremove && apt-get -y autoclean"
+
+#sudo /bin/sh -c "apt-get update && apt-get dist-upgrade && apt-get autoremove && apt-get autoclean"
